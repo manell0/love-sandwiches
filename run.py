1fe9bcf1ee1c3297259d64eb0b57ea761c366ec6
Svante Magnell <44971575+manell0@users.noreply.github.com>
2021-06-12 11:12:01 +0200

    Update with a function
    Add funtion for input (def get_sales_data():)

--- run.py
+++ run.py
@@ -13,8 +13,21 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('love_sandwiches')
 
-sales = SHEET.worksheet('sales')
+def get_sales_data():
+    """
+    Get sales figures input from the user
+    """
+    print('Please enter sales data from the last market.')
+    print('Data shuld be sic numbers, separated by commas.')
+    print('Example: 10,20,30,40,50,60\n')
 
-data = sales.get_all_values()
+    data_str = input('Enter your data here: ')
+    print(data_str)
 
-print(data)
+get_sales_data()
+
+#sales = SHEET.worksheet('sales')
+
+#data = sales.get_all_values()
+
+#print(data)
