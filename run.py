d8937de2a3d70ab47a1bec24378e0dae9d8f4d7a
Svante Magnell <44971575+manell0@users.noreply.github.com>
2021-06-13 10:15:38 +0200

    Update with function for validate data
    Inside the try, converts all string values into integers. Raises ValueError if strings cannot be converted into int, or if there aren't exactly 6 values.

--- run.py
+++ run.py
@@ -1,4 +1,3 @@
-  
 import gspread
 from google.oauth2.service_account import Credentials
 
@@ -6,28 +5,54 @@ SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
     "https://www.googleapis.com/auth/drive.file",
     "https://www.googleapis.com/auth/drive"
-    ]
+]
 
-CREDS = Credentials.from_service_account_file('creds.json')
-SCOPED_CREDS = CREDS.with_scopes(SCOPE)
-GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
-SHEET = GSPREAD_CLIENT.open('love_sandwiches')
+CREDS = Credentials.from_service_account_file ('creds.json')
+SCOPED_CREDS = CREDS.with_scopes (SCOPE)
+GSPREAD_CLIENT = gspread.authorize (SCOPED_CREDS)
+SHEET = GSPREAD_CLIENT.open ('love_sandwiches')
+
 
 def get_sales_data():
     """
     Get sales figures input from the user
     """
-    print('Please enter sales data from the last market.')
-    print('Data shuld be sic numbers, separated by commas.')
-    print('Example: 10,20,30,40,50,60\n')
+    print ('Please enter sales data from the last market.')
+    print ('Data should be six numbers, separated by commas.')
+    print ('Example: 10,20,30,40,50,60\n')
 
-    data_str = input('Enter your data here: ')
-    print(data_str)
+    # data_str = input('Enter your data here: ')
+    data_str = '1,2,3,5,6,8'
 
-get_sales_data()
+    sales_data = data_str.split (',')
+    validate_data (sales_data)
 
-#sales = SHEET.worksheet('sales')
 
-#data = sales.get_all_values()
+def validate_data(values):
 
-#print(data)
+    """
+    Inside the try, converts all string values into integers.
+    Raises ValueError if strings cannot be converted into int,
+    or if there aren't exactly 6 values.
+    """
+    try:
+        [int(value) for value in values]
+        if len (values) != 6:
+            raise ValueError (
+                f'Exactly SIX values, you provided {len (values)}'
+
+            )
+    except ValueError as e:
+        print (f'Invalid data: {e}, please try again.\n')
+    finally:
+        print (f'You entered: {values}')
+
+
+get_sales_data ()
+
+
+# sales = SHEET.worksheet('sales')
+
+# data = sales.get_all_values()
+
+# print(data)
